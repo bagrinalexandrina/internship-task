89bd1f9255c564170a8c6b328f6f43a1a359850d
Alexandrina <bagrin.alexandrina@gmail.com>
2021-10-13 13:22:57 +0300

    created the product serializer

--- webshop/products/models.py
+++ webshop/products/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.FloatField(help_text='in US dolars')
     description = models.CharField(max_length=400)
     created = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='products', default='no_picture.png')
 
     def __str__(self):
-        return f"{self.id}. Produs: {self.name}; {self.price}; {self.description}; {self.created.strftime('%d/%m/%y')}"
+        return f"{self.id}. {self.name} - {self.created.strftime('%d/%m/%y')}"

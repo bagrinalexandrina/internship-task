e7de0cf74ecd8c12b39e28981db3b6f755be8d00
Alexandrina <bagrin.alexandrina@gmail.com>
2021-10-16 20:36:19 +0300

    created the ProductImage model and serializer

--- webshop/products/models.py
+++ webshop/products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -10,3 +12,11 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.id}. {self.name} - {self.created.strftime('%d/%m/%y')}"
+
+
+class ProductImage(models.Model):
+    link = models.TextField()
+    product_image = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='image')
+
+    def __str__(self):
+        return f"{self.id}. {self.link}; {self.product_image}"
--- webshop/products/serializers.py
+++ webshop/products/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework import serializers
-from products.models import Product
+from rest_framework import fields, serializers
+from products.models import Product, ProductImage
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name', 'sku', 'price', 'description']
+        fields = ['name', 'sku', 'price', 'description', 'created']
 
+class ProductImageSerializer(ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ['link', 'product_image']
 
 

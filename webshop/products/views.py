8ee8b37d17aff7312289540e1f5c509b8cbc80b8
Alexandrina <bagrin.alexandrina@gmail.com>
2021-10-17 22:59:05 +0300

    edited the pagination and added photos

--- webshop/products/views.py
+++ webshop/products/views.py
@@ -7,7 +7,7 @@ from rest_framework import mixins
 from rest_framework import generics
 
 class ProductSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 3
     page_size_query_param = 'page_size'
     max_page_size = 100
 
